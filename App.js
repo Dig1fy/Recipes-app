7b89f3b72392b29668cda297212786c002b1e7e2
Ivaylo Petrov <ivaylozp@gmail.com>
2021-11-06 22:52:24 +0200

    Implement stack navigator. Add custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+// import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Fonts from 'expo-font'
+import AppLoading from 'expo-app-loading'
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Fonts.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>X</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
