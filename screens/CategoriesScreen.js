e50a26dc911ac118f6da44f274b831505c2624c5
Ivaylo Petrov <ivaylozp@gmail.com>
2021-11-07 13:37:52 +0200

    Work on navigating between screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+import { CATEGORIES } from './data/dummy-data';
 
 const CategoriesScreen = props => {
+
+    const renderGridItem = itemData => {
+        return (
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                    props.navigation.navigate('CategoryMeals')
+                }}>
+                <View >
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-        </View>
+        <FlatList
+            numColumns={2}
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            keyExtractor={(item) => item.id}
+        />
     )
+}
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meals Categories (nav options)',
+    headerStyle: {
+        backgroundColor: 'red'
+    }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +41,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 120,
     }
 })
 
