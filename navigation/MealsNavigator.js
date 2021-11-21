bd4249d418931e48688674d2884c3477427a9b3e
Ivaylo Petrov <ivaylozp@gmail.com>
2021-11-21 23:54:52 +0200

    Add Drawer navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
+
+// Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons, MaterialIcons, SimpleLineIcons } from '@expo/vector-icons'
+import { DrawerActions } from '@react-navigation/routers';
 
+// Screens
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
 import FavoritesScreen from '../screens/FavoritesScreen'
-import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+import FilterScreen from '../screens/FiltersScreen'
 
 import Colors from '../screens/constants/Colors';
 
@@ -18,7 +24,18 @@ const SecondStack = createNativeStackNavigator();
 const FavoritesStackNavigator = () => {
     return (
         <SecondStack.Navigator screenOptions={screenOptions}>
-            <SecondStack.Screen name="Favorites" component={FavoritesScreen} options={{ title: "Your favorites" }} />
+            <SecondStack.Screen name="Favorites" component={FavoritesScreen} options={{
+                title: "Your favorites",
+                headerLeft: () => (
+                    <SimpleLineIcons
+                        style={{ marginRight: 20 }}
+                        name="menu"
+                        size={30}
+                        color="#900"
+                        onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
+                    />
+                ),
+            }} />
             <SecondStack.Screen name="MealDetail" component={MealDetailScreen} />
         </SecondStack.Navigator>
     );
@@ -29,55 +46,85 @@ const Tab = createBottomTabNavigator();
 
 const FavoriteMealsTabNavigator = () => {
     return (
-        <NavigationContainer>
-            <Tab.Navigator screenOptions={{ headerShown: false }}>
-                <Tab.Screen name='Meals' component={NestedStackNavigator}
-                    options={() => ({
-                        tabBarIcon: ({ color, size }) => {
-                            let iconName = 'ios-restaurant'
-                            // We can return any component here!
-                            return <Ionicons name={iconName} size={25} color={color} />;
-                        },
-                        tabBarActiveTintColor: 'yellow',
-                        tabBarInactiveTintColor: 'gray',
-                        tabBarActiveBackgroundColor: Colors.primaryColor,
-                        tabBarInactiveBackgroundColor: Colors.primaryColor,
-                        tabBarLabelStyle: tabBarLabelStyle
-                    })}
-                />
-                <Tab.Screen name='FavStackNavigator' component={FavoritesStackNavigator}
-                    options={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName = 'star-half-outline'
-                            // We can return any component that here!
-                            return <Ionicons name={iconName} size={size} color={color} />;
-                        },
-                        title: "Favorites",
-                        headerShown: false,
-                        tabBarActiveTintColor: 'yellow',
-                        tabBarInactiveTintColor: 'gray',
-                        tabBarActiveBackgroundColor: Colors.primaryColor,
-                        tabBarInactiveBackgroundColor: Colors.primaryColor,
-                        tabBarLabelStyle: tabBarLabelStyle,
-                    })}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name='Meals' component={NestedStackNavigator}
+                options={() => ({
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName = 'ios-restaurant'
+                        // We can return any component here!
+                        return <Ionicons name={iconName} size={25} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'yellow',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarActiveBackgroundColor: Colors.primaryColor,
+                    tabBarInactiveBackgroundColor: Colors.primaryColor,
+                    tabBarLabelStyle: tabBarLabelStyle
+                })}
+            />
+            <Tab.Screen name='FavStackNavigator' component={FavoritesStackNavigator}
+                options={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName = 'star-half-outline'
+                        // We can return any component that here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    title: "Favorites",
+                    headerShown: false,
+                    tabBarActiveTintColor: 'yellow',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarActiveBackgroundColor: Colors.primaryColor,
+                    tabBarInactiveBackgroundColor: Colors.primaryColor,
+                    tabBarLabelStyle: tabBarLabelStyle,
+                })}
+            />
+        </Tab.Navigator>
     )
 }
 
 //NESTED Stack navigator
 const Stack = createNativeStackNavigator();
 
-const NestedStackNavigator = () => {
+const NestedStackNavigator = (props) => {
     return (
         <Stack.Navigator screenOptions={screenOptions}>
-            <Stack.Screen name="Categories" component={CategoriesScreen} />
+            <Stack.Screen name="Categories" component={CategoriesScreen} options={{
+                headerLeft: () => (
+                    <SimpleLineIcons
+                        style={{ marginRight: 20 }}
+                        name="menu"
+                        size={30}
+                        color="#900"
+                        onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
+                    />
+                ),
+            }} />
             <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
             <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
     );
 }
+
+const FilterStack = createNativeStackNavigator();
+const FilterStackNav = props => (
+    <FilterStack.Navigator>
+        <FilterStack.Screen name='FavoritesScreen' component={FilterScreen} />
+    </FilterStack.Navigator>
+)
+
+const Drawer = createDrawerNavigator();
+const DrawerNav = props => (
+    <NavigationContainer>
+        <Drawer.Navigator>
+            <Drawer.Screen name='MealsFavorites' component={FavoriteMealsTabNavigator} options={{ headerShown: false }} />
+            <Drawer.Screen name='Filters' component={FilterStackNav} options={{
+                headerTitle: 'Filter Meals',
+                headerShown: false
+            }} />
+        </Drawer.Navigator>
+    </NavigationContainer>
+)
+
+export default DrawerNav;
 
 //setting common screen options between the screens
 const screenOptions = {
@@ -96,5 +143,3 @@ const tabBarLabelStyle = {
     textShadowRadius: 1,
     opacity: 0.9
 }
-
-export default FavoriteMealsTabNavigator;
